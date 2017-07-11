99d86189a41566d82a48d4033b3b1722ba860f3a
mahmoud_abdel_latif <mahmoudabdellatif830@gmail.com>
2017-07-11 13:23:14 +0200

    last commit

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
+  resources :users
+  get 'store/index'
+
+  resources :products do
+	get :who_bought, on: :member
+  end
+  
+end
